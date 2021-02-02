b87ddd5a4efbd385c8d987822b29f5ad78aae1a9
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2021-02-02 21:46:09 +0100

    Create Kata 1

---
+++ src/Computation/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Computation
+{
+    public class Calculator
+    {
+        public int SumNumbers(int start, int count)
+        {
+            // TODO #1: Get all tests passing with a naive implementation.
+
+            // TODO #2: Refactor so there are no loops (while, for, foreach) or if statements.
+
+            // TODO #3: Refactor to use the Aggregate() LINQ method.
+            throw new NotImplementedException();
+        }
+    }
+}
