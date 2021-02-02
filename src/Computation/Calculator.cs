46e1859bff7322be1028d5bc158be6273f205990
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2021-02-02 21:46:09 +0100

    Create Kata 1

---
+++ src/Computation/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Computation
+{
+    public class Calculator
+    {
+        public int SumNumbers(int start, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
