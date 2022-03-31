b6112887b12ca1117e0e350fe375cf28945f7e85
Tony Bernis <46629609+TBernis-code@users.noreply.github.com>
2022-03-31 12:29:59 +0200

    first commit

---
+++ src/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : Controller
+    {
+        private repo = new PersonRepository();
+        
+        public ActionResult Person()
+        {
+            ViewBag.Test = "toto";
+            return View();
+        }
+
+    }
+}
